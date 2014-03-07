4e6508a997d3b2da2df88d269492264400c22e5e
Toshiaki Maki <makits@nttdata.co.jp>
2014-03-07 18:06:48 +0900

    Supported mybatis3 #16

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -10,6 +10,12 @@ mv src/main/resources/META-INF/spring/projectName-infra.xml src/main/resources/M
 mv src/main/resources/META-INF/spring/projectName-env.xml src/main/resources/META-INF/spring/__artifactId__-env.xml
 mv src/main/resources/META-INF/spring/projectName-infra.properties src/main/resources/META-INF/spring/__artifactId__-infra.properties
 
+if [ -d src/main/resources/xxxxxx ];then
+  echo "rename to __packageInPathFormat__"
+  mkdir -p src/main/resources/__packageInPathFormat__
+  mv src/main/resources/xxxxxx/yyyyyy/zzzzzz/domain src/main/resources/__packageInPathFormat__/
+  rm -rf src/main/resources/xxxxxx
+fi
 
 sed -i -e "s/org\.terasoluna\.gfw\.blank/xxxxxx.yyyyyy.zzzzzz/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/projectName/g" pom.xml
