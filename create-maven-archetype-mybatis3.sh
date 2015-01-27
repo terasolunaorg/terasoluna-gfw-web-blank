fb4856d05fa9c03e88cdc5eb86e743b8ca0f520c
Toshiaki Maki <makingx@gmail.com>
2015-01-27 13:40:12 +0900

    changed description #77

--- create-maven-archetype-mybatis3.sh
+++ create-maven-archetype-mybatis3.sh
@@ -2,5 +2,5 @@
 sh change-infra.sh MyBatis3
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-mybatis3/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-mybatis3/g" create-maven-archetype.sh
-sed -i -e "s/<description>Blank project using TERASOLUNA Global Framework/<description>Blank project using TERASOLUNA Global Framework (MyBatis3)/g" pom.xml
+sed -i -e "s/Web Blank Project/Web Blank Project (MyBatis3)/g" pom.xml
 sh create-maven-archetype.sh
