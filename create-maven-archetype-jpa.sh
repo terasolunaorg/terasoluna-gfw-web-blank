304fb8d1e76b926cce6be345ba14376e00b53227
bttanakagn <tanakagn@pm.nttdata.co.jp>
2016-08-02 22:04:05 +0900

    add double quote to the parameter #206

--- create-maven-archetype-jpa.sh
+++ create-maven-archetype-jpa.sh
@@ -3,4 +3,4 @@ sh change-infra.sh JPA
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-jpa/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-jpa/g" create-maven-archetype.sh
 sed -i -e "s/Web Blank Project/Web Blank Project (JPA)/g" pom.xml
-sh create-maven-archetype.sh $1
+sh create-maven-archetype.sh "$1"
