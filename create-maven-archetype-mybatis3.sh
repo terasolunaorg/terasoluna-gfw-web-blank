f5b0f6adc52c699ea7b01b8b45462480d39bd48b
bttanakagn <tanakagn@pm.nttdata.co.jp>
2016-08-02 21:54:37 +0900

    Make it possible to deploy to maven central #206

--- create-maven-archetype-mybatis3.sh
+++ create-maven-archetype-mybatis3.sh
@@ -3,4 +3,4 @@ sh change-infra.sh MyBatis3
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-mybatis3/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-mybatis3/g" create-maven-archetype.sh
 sed -i -e "s/Web Blank Project/Web Blank Project (MyBatis3)/g" pom.xml
-sh create-maven-archetype.sh
+sh create-maven-archetype.sh $1
--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -21,10 +21,54 @@ sed -i -e "s/org\.terasoluna\.gfw\.blank/xxxxxx.yyyyyy.zzzzzz/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/projectName/g" pom.xml
 
 rm -rf `find . -name '.svn' -type d`
-mvn archetype:create-from-project
+
+if [ $1 = "central" ]; then
+  profile="-P central"
+fi
+mvn archetype:create-from-project ${profile}
 
 pushd target/generated-sources/archetype
 
 sed -i -e "s/xxxxxx\.yyyyyy\.zzzzzz/org.terasoluna.gfw.blank/g" pom.xml
 sed -i -e "s/projectName/terasoluna-gfw-web-blank/g" pom.xml
+
+if [ $1 = "central" ]; then
+  # add plugins to deploy to Maven Central Repository
+  LF=$(printf '\\\012_')
+  LF=${LF%_}
+  
+  TAG_FEATURE_MANAGER="    <plugins>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}      <plugin>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <groupId>org.sonatype.plugins<\/groupId>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <artifactId>nexus-staging-maven-plugin<\/artifactId>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <version>1.6.7<\/version>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <extensions>true<\/extensions>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <configuration>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}          <serverId>ossrh<\/serverId>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}          <nexusUrl>https:\/\/oss.sonatype.org\/<\/nexusUrl>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}          <autoReleaseAfterClose>true<\/autoReleaseAfterClose>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <\/configuration>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}      <\/plugin>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}      <plugin>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <groupId>org.apache.maven.plugins<\/groupId>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <artifactId>maven-gpg-plugin<\/artifactId>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <version>1.6<\/version>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <executions>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}          <execution>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}            <id>sign-artifacts<\/id>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}            <phase>verify<\/phase>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}            <goals>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}              <goal>sign<\/goal>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}            <\/goals>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}          <\/execution>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}        <\/executions>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}      <\/plugin>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}    <\/plugins>${LF}"
+  TAG_FEATURE_MANAGER="${TAG_FEATURE_MANAGER}  <\/build>"
+  
+  sed -i -e "s/  <\/build>/${TAG_FEATURE_MANAGER}/" pom.xml
+fi
 mvn deploy
+
+popd
+popd
