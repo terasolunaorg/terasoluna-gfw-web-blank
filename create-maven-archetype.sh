58bc70903ee975964164ccfcbfa7c92e3c85662b
btryuushou <ryuusuj@pm.nttdata.co.jp>
2016-10-03 11:26:29 +0900

    Resolve the difference between the settings of web-multi-blank #237
    (cherry picked from commit 985e7d18cdc82901b79e9376a0ea98cf38e75a33)

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -12,34 +12,12 @@ mv src/main/resources/META-INF/spring/projectName-codelist.xml src/main/resource
 # if JPA or Mybatis2 is used
 if [ -e src/main/resources/META-INF/spring/projectName-env.xml ];then
   mv src/main/resources/META-INF/spring/projectName-env.xml src/main/resources/META-INF/spring/__artifactId__-env.xml
-
-  # add database dependency info
-  LF=$(printf '\\\012_')
-  LF=${LF%_}
-
-  REPLACEMENT_TAG="        <\!-- \=\= Begin Database \=\= -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--         <dependency> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--             <groupId>org.postgresql<\/groupId> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--             <artifactId>postgresql<\/artifactId> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--             <version>\$\{postgresql.version\}<\/version> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--         </dependency> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--         <dependency> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--             <groupId>com.oracle<\/groupId> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--             <artifactId>ojdbc7<\/artifactId> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--             <version>\$\{ojdbc.version\}<\/version> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}<\!--         <\/dependency> -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <\!-- \=\= End Database \=\= -->${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}    <\/dependencies>"
-
-  sed -i -e "s/    <\/dependencies>/${REPLACEMENT_TAG}/" pom.xml
-
-  REPLACEMENT_TAG="        <postgresql.version>9.4-1206-jdbc41<\/postgresql.version>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <ojdbc.version>12.1.0.2<\/ojdbc.version>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <selenium.version>2.46.0<\/selenium.version>"
-
-  sed -i -e "s/        <selenium.version>2.46.0<\/selenium.version>/${REPLACEMENT_TAG}/" pom.xml
-
+else
+  startLine=`sed -n '/Begin Database/=' pom.xml`
+  endLine=`sed -n '/End Database/=' pom.xml`
+  sed -i $startLine','$endLine'd' pom.xml
+  sed -i '/<postgresql.version>/d' pom.xml
+  sed -i '/<ojdbc.version>/d' pom.xml
 fi
 if [ -e src/main/resources/META-INF/spring/projectName-infra.properties ];then
   mv src/main/resources/META-INF/spring/projectName-infra.properties src/main/resources/META-INF/spring/__artifactId__-infra.properties
