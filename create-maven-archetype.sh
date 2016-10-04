45988b2c62a65133177762d8d7d69ebde151aea8
btryuushou <ryuusuj@pm.nttdata.co.jp>
2016-10-04 18:08:22 +0900

    Resolve the difference between the settings of web-multi-blank #237
    (cherry picked from commit d536def909baa44d4616bf10e97e06071785769b)

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -9,8 +9,8 @@ mv src/main/resources/META-INF/spring/projectName-domain.xml src/main/resources/
 mv src/main/resources/META-INF/spring/projectName-infra.xml src/main/resources/META-INF/spring/__artifactId__-infra.xml
 mv src/main/resources/META-INF/spring/projectName-codelist.xml src/main/resources/META-INF/spring/__artifactId__-codelist.xml
 
-# if JPA or Mybatis2 is used
-if [ -e src/main/resources/META-INF/spring/projectName-env.xml ];then
+# if JPA or Mybatis3 is used
+if [ -e src/main/resources/META-INF/spring/projectName-env.xml ]; then
   mv src/main/resources/META-INF/spring/projectName-env.xml src/main/resources/META-INF/spring/__artifactId__-env.xml
 else
   startLine=`sed -n '/Begin Database/=' pom.xml`
@@ -19,10 +19,16 @@ else
   sed -i '/<postgresql.version>/d' pom.xml
   sed -i '/<ojdbc.version>/d' pom.xml
 fi
-if [ -e src/main/resources/META-INF/spring/projectName-infra.properties ];then
+if [ -e src/main/resources/META-INF/spring/projectName-infra.properties ]; then
   mv src/main/resources/META-INF/spring/projectName-infra.properties src/main/resources/META-INF/spring/__artifactId__-infra.properties
 fi
 
+if [ -d src/main/resources/xxxxxx ]; then
+  echo "rename to __packageInPathFormat__"
+  mkdir -p src/main/resources/__packageInPathFormat__
+  mv src/main/resources/xxxxxx/yyyyyy/zzzzzz/domain src/main/resources/__packageInPathFormat__/
+  rm -rf src/main/resources/xxxxxx
+fi
 
 sed -i -e "s/org\.terasoluna\.gfw\.blank/xxxxxx.yyyyyy.zzzzzz/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/projectName/g" pom.xml
