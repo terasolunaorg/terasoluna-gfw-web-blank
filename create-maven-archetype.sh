b352e2bc3dfc9e0c29022e9f3731ca9a9c99f6cb
btryuushou <ryuusuj@pm.nttdata.co.jp>
2016-10-04 19:06:52 +0900

    Resolve the difference between the settings of web-multi-blank #237
    (cherry picked from commit e875fdda1587a1d7990204bb218cf3f6a3d78be6)

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -9,6 +9,12 @@ mv src/main/resources/META-INF/spring/projectName-domain.xml src/main/resources/
 mv src/main/resources/META-INF/spring/projectName-infra.xml src/main/resources/META-INF/spring/__artifactId__-infra.xml
 mv src/main/resources/META-INF/spring/projectName-codelist.xml src/main/resources/META-INF/spring/__artifactId__-codelist.xml
 
+startLine=`sed -n '/Begin Database/=' pom.xml`
+endLine=`sed -n '/End Database/=' pom.xml`
+sed -i $startLine','$endLine'd' pom.xml
+sed -i '/<postgresql.version>/d' pom.xml
+sed -i '/<ojdbc.version>/d' pom.xml
+
 # if JPA or Mybatis3 is used
 if [ -e src/main/resources/META-INF/spring/projectName-env.xml ];then
   mv src/main/resources/META-INF/spring/projectName-env.xml src/main/resources/META-INF/spring/__artifactId__-env.xml
@@ -34,15 +40,6 @@ pushd target/generated-sources/archetype
 
 sed -i -e "s/xxxxxx\.yyyyyy\.zzzzzz/org.terasoluna.gfw.blank/g" pom.xml
 sed -i -e "s/projectName/terasoluna-gfw-web-blank/g" pom.xml
-
-# if JPA or Mybatis3 is used
-# if [ -e src/main/resources/META-INF/spring/__artifactId__-env.xml ];then
-  startLine=`sed -n '/Begin Database/=' pom.xml`
-  endLine=`sed -n '/End Database/=' pom.xml`
-  sed -i $startLine','$endLine'd' pom.xml
-  sed -i '/<postgresql.version>/d' pom.xml
-  sed -i '/<ojdbc.version>/d' pom.xml
-# fi
 
 if [ "$1" = "central" ]; then
   # add plugins to deploy to Maven Central Repository
