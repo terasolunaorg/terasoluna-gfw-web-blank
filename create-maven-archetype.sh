2f8ea4f5c6ff40b537135eb62fae4d476cd68fef
btryuushou <ryuusuj@pm.nttdata.co.jp>
2016-10-04 18:35:28 +0900

    Resolve the difference between the settings of web-multi-blank #237
    (cherry picked from commit 640ab649545006a363dc7c79684d1bd2fe23e4c5)

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -10,20 +10,14 @@ mv src/main/resources/META-INF/spring/projectName-infra.xml src/main/resources/M
 mv src/main/resources/META-INF/spring/projectName-codelist.xml src/main/resources/META-INF/spring/__artifactId__-codelist.xml
 
 # if JPA or Mybatis3 is used
-if [ -e src/main/resources/META-INF/spring/projectName-env.xml ]; then
+if [ -e src/main/resources/META-INF/spring/projectName-env.xml ];then
   mv src/main/resources/META-INF/spring/projectName-env.xml src/main/resources/META-INF/spring/__artifactId__-env.xml
-else
-  startLine=`sed -n '/Begin Database/=' pom.xml`
-  endLine=`sed -n '/End Database/=' pom.xml`
-  sed -i $startLine','$endLine'd' pom.xml
-  sed -i '/<postgresql.version>/d' pom.xml
-  sed -i '/<ojdbc.version>/d' pom.xml
 fi
-if [ -e src/main/resources/META-INF/spring/projectName-infra.properties ]; then
+if [ -e src/main/resources/META-INF/spring/projectName-infra.properties ];then
   mv src/main/resources/META-INF/spring/projectName-infra.properties src/main/resources/META-INF/spring/__artifactId__-infra.properties
 fi
 
-if [ -d src/main/resources/xxxxxx ]; then
+if [ -d src/main/resources/xxxxxx ];then
   echo "rename to __packageInPathFormat__"
   mkdir -p src/main/resources/__packageInPathFormat__
   mv src/main/resources/xxxxxx/yyyyyy/zzzzzz/domain src/main/resources/__packageInPathFormat__/
@@ -40,4 +34,51 @@ pushd target/generated-sources/archetype
 
 sed -i -e "s/xxxxxx\.yyyyyy\.zzzzzz/org.terasoluna.gfw.blank/g" pom.xml
 sed -i -e "s/projectName/terasoluna-gfw-web-blank/g" pom.xml
+
+# if JPA or Mybatis3 is used
+if [ -e src/main/resources/META-INF/spring/__artifactId__-env.xml ];then
+  startLine=`sed -n '/Begin Database/=' pom.xml`
+  endLine=`sed -n '/End Database/=' pom.xml`
+  sed -i $startLine','$endLine'd' pom.xml
+  sed -i '/<postgresql.version>/d' pom.xml
+  sed -i '/<ojdbc.version>/d' pom.xml
+fi
+
+if [ "$1" = "central" ]; then
+  # add plugins to deploy to Maven Central Repository
+  LF=$(printf '\\\012_')
+  LF=${LF%_}
+  
+  REPLACEMENT_TAG="    <plugins>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <plugin>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <groupId>org.sonatype.plugins<\/groupId>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <artifactId>nexus-staging-maven-plugin<\/artifactId>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <version>1.6.7<\/version>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <extensions>true<\/extensions>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <configuration>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <serverId>ossrh<\/serverId>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <nexusUrl>https:\/\/oss.sonatype.org\/<\/nexusUrl>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <autoReleaseAfterClose>true<\/autoReleaseAfterClose>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <\/configuration>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <\/plugin>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <plugin>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <groupId>org.apache.maven.plugins<\/groupId>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <artifactId>maven-gpg-plugin<\/artifactId>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <version>1.6<\/version>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <executions>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <execution>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <id>sign-artifacts<\/id>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <phase>verify<\/phase>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <goals>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}              <goal>sign<\/goal>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <\/goals>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <\/execution>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <\/executions>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <\/plugin>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}    <\/plugins>${LF}"
+  REPLACEMENT_TAG="${REPLACEMENT_TAG}  <\/build>"
+  
+  sed -i -e "s/  <\/build>/${REPLACEMENT_TAG}/" pom.xml
+fi
+
 mvn deploy
