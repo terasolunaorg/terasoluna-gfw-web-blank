5654a27285c9a4b832f32fea2813bcf58b785baf
btryuushou <ryuusuj@pm.nttdata.co.jp>
2016-10-07 19:12:23 +0900

    modified the difference between the settings of web-multi-blank #237
    (cherry picked from commit e9c9ede3f6912729a8df4f70b3af49d5ee84fe7a)

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -4,6 +4,14 @@ mkdir tmp
 cp -r src pom.xml tmp
 pushd tmp
 
+# delete database info if JPA or Mybatis3 is not used
+grep "<artifactId>" pom.xml | head -1 | grep -E "jpa|mybatis3" >/dev/null
+if [ $? -ne 0 ]; then
+  sed -i -e '/Begin Database/,/End Database/d' pom.xml
+  sed -i -e '/postgresql.version/d' pom.xml
+  sed -i -e '/ojdbc.version/d' pom.xml
+fi
+
 # rename "projectName" in filename to replace by ${artifactId}
 mv src/main/resources/META-INF/spring/projectName-domain.xml src/main/resources/META-INF/spring/__artifactId__-domain.xml
 mv src/main/resources/META-INF/spring/projectName-infra.xml src/main/resources/META-INF/spring/__artifactId__-infra.xml
@@ -12,12 +20,6 @@ mv src/main/resources/META-INF/spring/projectName-codelist.xml src/main/resource
 # if JPA or Mybatis3 is used
 if [ -e src/main/resources/META-INF/spring/projectName-env.xml ];then
   mv src/main/resources/META-INF/spring/projectName-env.xml src/main/resources/META-INF/spring/__artifactId__-env.xml
-else
-  startLine=`sed -n '/Begin Database/=' pom.xml`
-  endLine=`sed -n '/End Database/=' pom.xml`
-  sed -i -e $startLine','$endLine'd' pom.xml
-  sed -i -e '/postgresql.version/d' pom.xml
-  sed -i -e '/ojdbc.version/d' pom.xml
 fi
 if [ -e src/main/resources/META-INF/spring/projectName-infra.properties ];then
   mv src/main/resources/META-INF/spring/projectName-infra.properties src/main/resources/META-INF/spring/__artifactId__-infra.properties
