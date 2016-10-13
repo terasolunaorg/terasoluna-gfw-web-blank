a4409d3eb25dd1e14aca3497b3934d24cf667965
btryuushou <ryuusuj@pm.nttdata.co.jp>
2016-10-13 16:53:42 +0900

    modified the difference between the settings of web-multi-blank after cherry-pick #237

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -4,8 +4,8 @@ mkdir tmp
 cp -r src pom.xml tmp
 pushd tmp
 
-# delete database info if JPA or Mybatis3 is not used
-grep "<artifactId>" pom.xml | head -1 | grep -E "jpa|mybatis3" >/dev/null
+# delete database info if JPA or Mybatis2 is not used
+grep "<artifactId>" pom.xml | head -1 | grep -E "jpa|mybatis2" >/dev/null
 if [ $? -ne 0 ]; then
   sed -i -e '/Begin Database/,/End Database/d' pom.xml
   sed -i -e '/postgresql.version/d' pom.xml
@@ -17,7 +17,7 @@ mv src/main/resources/META-INF/spring/projectName-domain.xml src/main/resources/
 mv src/main/resources/META-INF/spring/projectName-infra.xml src/main/resources/META-INF/spring/__artifactId__-infra.xml
 mv src/main/resources/META-INF/spring/projectName-codelist.xml src/main/resources/META-INF/spring/__artifactId__-codelist.xml
 
-# if JPA or Mybatis3 is used
+# if JPA or Mybatis2 is used
 if [ -e src/main/resources/META-INF/spring/projectName-env.xml ];then
   mv src/main/resources/META-INF/spring/projectName-env.xml src/main/resources/META-INF/spring/__artifactId__-env.xml
 fi
@@ -25,12 +25,6 @@ if [ -e src/main/resources/META-INF/spring/projectName-infra.properties ];then
   mv src/main/resources/META-INF/spring/projectName-infra.properties src/main/resources/META-INF/spring/__artifactId__-infra.properties
 fi
 
-if [ -d src/main/resources/xxxxxx ];then
-  echo "rename to __packageInPathFormat__"
-  mkdir -p src/main/resources/__packageInPathFormat__
-  mv src/main/resources/xxxxxx/yyyyyy/zzzzzz/domain src/main/resources/__packageInPathFormat__/
-  rm -rf src/main/resources/xxxxxx
-fi
 
 sed -i -e "s/org\.terasoluna\.gfw\.blank/xxxxxx.yyyyyy.zzzzzz/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/projectName/g" pom.xml
@@ -42,45 +36,4 @@ pushd target/generated-sources/archetype
 
 sed -i -e "s/xxxxxx\.yyyyyy\.zzzzzz/org.terasoluna.gfw.blank/g" pom.xml
 sed -i -e "s/projectName/terasoluna-gfw-web-blank/g" pom.xml
-
-if [ "$1" = "central" ]; then
-  # add plugins to deploy to Maven Central Repository
-  LF=$(printf '\\\012_')
-  LF=${LF%_}
-  
-  REPLACEMENT_TAG="    <plugins>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <plugin>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <groupId>org.sonatype.plugins<\/groupId>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <artifactId>nexus-staging-maven-plugin<\/artifactId>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <version>1.6.7<\/version>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <extensions>true<\/extensions>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <configuration>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <serverId>ossrh<\/serverId>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <nexusUrl>https:\/\/oss.sonatype.org\/<\/nexusUrl>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <autoReleaseAfterClose>true<\/autoReleaseAfterClose>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <\/configuration>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <\/plugin>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <plugin>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <groupId>org.apache.maven.plugins<\/groupId>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <artifactId>maven-gpg-plugin<\/artifactId>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <version>1.6<\/version>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <executions>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <execution>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <id>sign-artifacts<\/id>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <phase>verify<\/phase>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <goals>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}              <goal>sign<\/goal>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}            <\/goals>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}          <\/execution>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}        <\/executions>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}      <\/plugin>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}    <\/plugins>${LF}"
-  REPLACEMENT_TAG="${REPLACEMENT_TAG}  <\/build>"
-  
-  sed -i -e "s/  <\/build>/${REPLACEMENT_TAG}/" pom.xml
-fi
-
 mvn deploy
-
-popd
-popd
