fbab6e240660013567f3ebddfcfa8abf63b89080
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2017-12-01 22:04:31 +0900

    be able to select install in addition to deploy (mybatis2) #333

--- create-maven-archetype-mybatis2.sh
+++ create-maven-archetype-mybatis2.sh
@@ -3,4 +3,4 @@ sh change-infra.sh MyBatis2
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-mybatis2/g" pom.xml
 sed -i -e "s/terasoluna-gfw-web-blank/terasoluna-gfw-web-blank-mybatis2/g" create-maven-archetype.sh
 sed -i -e "s/Web Blank Project/Web Blank Project (MyBatis2)/g" pom.xml
-sh create-maven-archetype.sh
+sh create-maven-archetype.sh "$1"
