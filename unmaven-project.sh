15bf125bd7ed7568fc3b1caeff86fe3740f97731
btkobayashirun <kobayashirun@pm.nttdata.co.jp>
2020-06-29 12:14:55 +0900

    fix scope compile from to runtime in shell of making wtp #437 (#438)
    * fix scope compile from to runtime in shell of making wtp #437
    
    * remove not working option #437
    
    * fix unification of options #437
    
    * remove option for libsrc setting #437
    
    * remove duplicate commands #437
    
    * fix the deletion order according to the mvn commands #437

--- unmaven-project.sh
+++ unmaven-project.sh
@@ -28,11 +28,10 @@ BUILDXML=build.xml
 ##-------------------------
 
 download_jars() {
-    rm -rf $WEBINFLIB $LIBSRC $TESTLIB
-    mvn dependency:copy-dependencies -DoutputDirectory=$WEBINFLIB -DincludeScope=compile -DexcludeScope=provided -DexcludeTypes=pom
-    mvn dependency:copy-dependencies -DoutputDirectory=$LIBSRC -DincludeScope=compile -DexcludeScope=provided -DexcludeTypes=pom -Dclassifier=sources
-    mvn dependency:copy-dependencies -DoutputDirectory=$TESTLIB -DexcludeScope=compile -DexcludeTypes=pom
-    mvn dependency:copy-dependencies -DoutputDirectory=$LIBSRC -DexcludeScope=compile -DexcludeTypes=pom -Dclassifier=sources
+    rm -rf $WEBINFLIB $TESTLIB $LIBSRC
+    mvn dependency:copy-dependencies -DoutputDirectory=$WEBINFLIB -DincludeScope=runtime -DexcludeTypes=pom
+    mvn dependency:copy-dependencies -DoutputDirectory=$TESTLIB -DexcludeScope=runtime -DexcludeTypes=pom
+    mvn dependency:copy-dependencies -DoutputDirectory=$LIBSRC -DexcludeTypes=pom -Dclassifier=sources
     echo $? > /dev/null
 }
 
