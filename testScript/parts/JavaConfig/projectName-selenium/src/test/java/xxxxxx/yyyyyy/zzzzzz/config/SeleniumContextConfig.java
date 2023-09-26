f441b13494ceaf241ca00707079417cf411c6066
SEH 大友 勇輝/Otomo <yuki.otomo.bp@jp.nttdata.com>
2023-09-26 20:13:03 +0900

    add WebDriverOperations

--- testScript/parts/JavaConfig/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/config/SeleniumContextConfig.java
+++ testScript/parts/JavaConfig/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/config/SeleniumContextConfig.java
@@ -5,9 +5,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.Resource;
+
+import xxxxxx.yyyyyy.zzzzzz.selenium.webdrivers.ChromeDriverFactoryBean;
+import xxxxxx.yyyyyy.zzzzzz.selenium.webdrivers.EdgeDriverFactoryBean;
+import xxxxxx.yyyyyy.zzzzzz.selenium.webdrivers.FirefoxDriverFactoryBean;
 
 /**
  * Bean definition to SeleniumContext configure .
@@ -30,12 +35,46 @@ public class SeleniumContextConfig {
 
     /**
      * Configure {@link WebDriver} bean.
-     * @return Bean of configured {@link FirefoxDriver}
+     * @return Bean of configured {@link FirefoxDriverFactoryBean}
      */
     @Bean("webDriver")
     @Scope("prototype")
-    public WebDriver webDriver() {
-        return new FirefoxDriver();
+    @Profile("default")
+    public FirefoxDriverFactoryBean defaultDriverFactoryBean() {
+        return new FirefoxDriverFactoryBean();
+    }
+
+    /**
+     * Configure {@link WebDriver} bean.
+     * @return Bean of configured {@link FirefoxDriverFactoryBean}
+     */
+    @Bean("webDriver")
+    @Scope("prototype")
+    @Profile("firefox")
+    public FirefoxDriverFactoryBean firefoxDriverFactoryBean() {
+        return new FirefoxDriverFactoryBean();
+    }
+
+    /**
+     * Configure {@link WebDriver} bean.
+     * @return Bean of configured {@link ChromeDriverFactoryBean}
+     */
+    @Bean("webDriver")
+    @Scope("prototype")
+    @Profile("chrome")
+    public ChromeDriverFactoryBean chromeDriverFactoryBean() {
+        return new ChromeDriverFactoryBean();
+    }
+
+    /**
+     * Configure {@link WebDriver} bean.
+     * @return Bean of configured {@link EdgeDriverFactoryBean}
+     */
+    @Bean("webDriver")
+    @Scope("prototype")
+    @Profile("edge")
+    public EdgeDriverFactoryBean edgeDriverFactoryBean() {
+        return new EdgeDriverFactoryBean();
     }
 
 }
