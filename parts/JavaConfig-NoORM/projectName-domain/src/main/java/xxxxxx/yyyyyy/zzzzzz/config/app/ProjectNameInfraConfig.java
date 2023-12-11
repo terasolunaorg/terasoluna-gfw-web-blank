f13859bced57fa7f2e02cf3673f160910e57a743
btonokit <Keita.Ono.bp@nttdata.com>
2023-12-11 18:44:42 +0900

    update boot to 3.2.0 #583

--- parts/JavaConfig-NoORM/projectName-domain/src/main/java/xxxxxx/yyyyyy/zzzzzz/config/app/ProjectNameInfraConfig.java
+++ parts/JavaConfig-NoORM/projectName-domain/src/main/java/xxxxxx/yyyyyy/zzzzzz/config/app/ProjectNameInfraConfig.java
@@ -1,8 +1,6 @@
 package xxxxxx.yyyyyy.zzzzzz.config.app;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 /**
  * Bean definitions for infrastructure layer.
