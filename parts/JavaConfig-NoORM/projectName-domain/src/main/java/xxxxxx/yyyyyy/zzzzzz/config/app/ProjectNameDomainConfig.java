82e3c499be0de0ff158eababe0663f81957c46c0
btsekinedaisuke <Daisuke11.Sekine.bp@jp.nttdata.com>
2023-12-22 16:38:22 +0900

    The JavaConfig description method has been unified. #585

--- parts/JavaConfig-NoORM/projectName-domain/src/main/java/xxxxxx/yyyyyy/zzzzzz/config/app/ProjectNameDomainConfig.java
+++ parts/JavaConfig-NoORM/projectName-domain/src/main/java/xxxxxx/yyyyyy/zzzzzz/config/app/ProjectNameDomainConfig.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright(c) 2023 NTT Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package xxxxxx.yyyyyy.zzzzzz.config.app;
 
 import org.springframework.aop.Advisor;
@@ -14,7 +29,7 @@ import org.terasoluna.gfw.common.exception.ResultMessagesLoggingInterceptor;
  * Bean definitions for domain layer.
  */
 @Configuration
-@ComponentScan(basePackages = {"xxxxxx.yyyyyy.zzzzzz.domain"})
+@ComponentScan(basePackages = { "xxxxxx.yyyyyy.zzzzzz.domain" })
 @Import({ ProjectNameInfraConfig.class,
     ProjectNameCodeListConfig.class })
 public class ProjectNameDomainConfig {
@@ -22,11 +37,12 @@ public class ProjectNameDomainConfig {
     /**
      * Configure messages logging AOP.
      * @param exceptionLogger Bean defined by ApplicationContext#exceptionLogger
-     * @see xxxxxx.yyyyyy.zzzzzz.config.app.ApplicationContext#exceptionLogger(ExceptionCodeResolver)
+     * @see xxxxxx.yyyyyy.zzzzzz.config.app.ApplicationContext#exceptionLogger()
      * @return Bean of configured {@link ResultMessagesLoggingInterceptor}
      */
     @Bean("resultMessagesLoggingInterceptor")
-    public ResultMessagesLoggingInterceptor resultMessagesLoggingInterceptor(ExceptionLogger exceptionLogger) {
+    public ResultMessagesLoggingInterceptor resultMessagesLoggingInterceptor(
+            ExceptionLogger exceptionLogger) {
         ResultMessagesLoggingInterceptor bean = new ResultMessagesLoggingInterceptor();
         bean.setExceptionLogger(exceptionLogger);
         return bean;
@@ -39,9 +55,11 @@ public class ProjectNameDomainConfig {
      * @return Advisor configured for PointCut
      */
     @Bean
-    public Advisor resultMessagesLoggingInterceptorAdvisor(ResultMessagesLoggingInterceptor resultMessagesLoggingInterceptor) {
-       AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
-       pointcut.setExpression("@within(org.springframework.stereotype.Service)");
-       return new DefaultPointcutAdvisor(pointcut, resultMessagesLoggingInterceptor);
+    public Advisor resultMessagesLoggingInterceptorAdvisor(
+            ResultMessagesLoggingInterceptor resultMessagesLoggingInterceptor) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(
+                "@within(org.springframework.stereotype.Service)");
+        return new DefaultPointcutAdvisor(pointcut, resultMessagesLoggingInterceptor);
     }
 }
