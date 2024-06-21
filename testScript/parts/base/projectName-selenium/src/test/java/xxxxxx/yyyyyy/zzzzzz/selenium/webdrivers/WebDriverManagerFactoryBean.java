da9b87152eec11cf33ae63242ffcccbfe20f7981
btkatoufm <66342857+btkatoufm@users.noreply.github.com>
2024-06-21 13:26:03 +0900

    [5.7.x] Make it headless compatible. #627 (#628)
    * The process around DRIVER was reviewed. Also, added headless option. #627
    
    * Fixed to JDK8 writing style. #627

--- testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/webdrivers/WebDriverManagerFactoryBean.java
+++ testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/webdrivers/WebDriverManagerFactoryBean.java
@@ -1,42 +1,44 @@
-/*
- * Copyright(c) 2023 NTT DATA Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
 package xxxxxx.yyyyyy.zzzzzz.selenium.webdrivers;
 
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.InitializingBean;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public abstract class WebDriverManagerFactoryBean<T extends WebDriver>
-                                                 implements FactoryBean<T>,
-                                                 InitializingBean {
+                                                 implements FactoryBean<T> {
 
-    private String propertyFileLocation;
+    protected String propertyFileLocation;
 
     public void setPropertyFileLocation(String propertyFileLocation) {
         this.propertyFileLocation = propertyFileLocation;
     }
 
+    protected abstract WebDriverManager getWebDriverManager();
+
+    protected abstract T createWebDriver();
+
     @Override
-    public void afterPropertiesSet() throws Exception {
-        if (propertyFileLocation != null) {
-            WebDriverManager.firefoxdriver().config().setProperties(
-                    propertyFileLocation);
+    public T getObject() {
+        if (System.getenv("webdriver.driver") == null) {
+            WebDriverManager manager = getWebDriverManager();
+            if (this.propertyFileLocation != null) {
+                manager.config().setProperties(this.propertyFileLocation);
+            }
+            manager.setup();
         }
+
+        return createWebDriver();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return WebDriver.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
     }
 
 }
