d5aecfef720cca2c8acd1e6a9c8893e3fe4eb6cb
btkatoufm <Fumio.Kato.bp@nttdata.com>
2024-07-18 13:47:32 +0900

    format. #632

--- change-infra.sh
+++ change-infra.sh
@@ -4,7 +4,7 @@ set -e
 MODE=$1
 KEYWORD="REMOVE THIS LINE IF YOU USE $1"
 TARGET="src pom.xml"
-DIRNAME=`echo $MODE | tr "[:upper:]" "[:lower:]"`
+DIRNAME=$(echo $MODE | tr "[:upper:]" "[:lower:]")
 
 echo "change to $MODE"
 
@@ -12,11 +12,10 @@ rm -rf tmp
 mkdir tmp
 cp -r infra/$DIRNAME/* tmp/
 cp -r infra/common/* tmp/
-rm -rf `/bin/find tmp -name '.svn' -type d `
+rm -rf $(/bin/find tmp -name '.svn' -type d)
 
 echo "copy infra/$DIRNAME to src/main/resources"
 cp -rf tmp/* src/main/resources
 rm -rf tmp
 
-
-sed -i -e "/$KEYWORD/d" `grep -rIl "$1" $TARGET | grep -v '.svn'`
+sed -i -e "/$KEYWORD/d" $(grep -rIl "$1" $TARGET | grep -v '.svn')
